9238a7d056e0036bdfe81ee8e6aba72353342acc
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-05-31 20:09:09 +0600

    add auth and firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFobG0wt00SpvTkJ0lCQ1S_WHTq_fnkps",
+  authDomain: "fitness-tracker-f5341.firebaseapp.com",
+  projectId: "fitness-tracker-f5341",
+  storageBucket: "fitness-tracker-f5341.appspot.com",
+  messagingSenderId: "903159765183",
+  appId: "1:903159765183:web:579494945b62a48da14e0a",
+};
+
+// Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/providers/AuthProviders.jsx
@@ -0,0 +1,70 @@
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+export const AuthContext = createContext(null);
+const AuthProviders = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const login = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+  //social login
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  const userInfo = {
+    createUser,
+    user,
+    loading,
+    login,
+    logout,
+    googleLogin,
+    updateUserProfile,
+    setLoading,
+  };
+  return (
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProviders;
