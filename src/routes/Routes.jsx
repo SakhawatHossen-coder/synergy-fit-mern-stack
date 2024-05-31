6d17fd18927b38663f8bbb9baa7bde5946d4dd9f
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-05-31 20:02:16 +0600

    basic routes and page setup

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import ErrorPage from "../pages/ErrorPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
