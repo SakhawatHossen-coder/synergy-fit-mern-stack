edda0ad4831200d8bf20cea49b3eec5d2cdb4378
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-06-01 20:01:47 +0600

    about section done

---
+++ src/components/About.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { FaSuperpowers } from "react-icons/fa";
+
+const About = () => {
+  return (
+    <div>
+      <section class="relative pt-12 bg-blueGray-50">
+        <div class="items-center flex flex-wrap">
+          <div class="sm:w-1/2 p-10">
+            <div class="image object-center text-center">
+              <img src="https://i.imgur.com/WbQnbas.png" />
+            </div>
+          </div>
+          <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
+            <div class="md:pr-12">
+              <div class="text-pink-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300 mt-8">
+                <i class="fas fa-rocket text-xl"></i>
+              </div>
+              <h3 class="text-3xl font-semibold">
+                About{" "}
+                <span className="text-teal-400 font-semibold">SynergyFit</span>:
+                Your Partner in Transformation
+              </h3>
+              <p class="mt-4 text-lg leading-relaxed text-blueGray-500">
+                we're passionate about igniting a revolution in health and
+                wellness. We believe everyone deserves to lead a vibrant, active
+                life, and we're dedicated to providing the tools and support to
+                make it happen.
+              </p>
+              <ul class="list-none mt-6">
+                <li class="py-2">
+                  <div class="flex items-center">
+                    <div>
+                      <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                        <i class="fas fa-fingerprint"></i>
+                      </span>
+                    </div>
+                    <div>
+                      <h4 class="text-blueGray-500">
+                        Innovation : Pioneering the development of
+                        next-generation solutions.
+                      </h4>
+                    </div>
+                  </div>
+                </li>
+                <li class="py-2">
+                  <div class="flex items-center">
+                    <div>
+                      <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                        <FaSuperpowers />
+                      </span>
+                    </div>
+                    <div>
+                      <h4 class="text-blueGray-500">
+                        Empowerment: Equipping you with the resources to unlock
+                        your full potential.
+                      </h4>
+                    </div>
+                  </div>
+                </li>
+                <li class="py-2">
+                  <div class="flex items-center">
+                    <div>
+                      <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                        <i class="far fa-paper-plane"></i>
+                      </span>
+                    </div>
+                    <div>
+                      <h4 class="text-blueGray-500">
+                        Inspiration: Fostering a motivating environment to fuel
+                        your journey.
+                      </h4>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Nav from "../components/Nav";
 import Banner from "../components/Banner";
 import Feature from "../components/Feature";
+import About from "../components/About";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
       <Feature />
+      <About/>
     </div>
   );
 };
