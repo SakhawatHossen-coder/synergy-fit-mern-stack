7cde57c72178eeb7399e6e042493c81fe016045e
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-06-04 21:30:09 +0600

    newsletter showns

--- src/dashboard/DashStat.jsx
+++ src/dashboard/DashStat.jsx
@@ -1,12 +1,24 @@
-import { Typography } from "@material-tailwind/react";
+import { Spinner, Typography } from "@material-tailwind/react";
 import React from "react";
+import useAxios from "../hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
 
 const DashStat = () => {
-  
+  let axiosCommon = useAxios();
+
+  const { data: subscribers = [], isLoading } = useQuery({
+    queryKey: ["subscribers"],
+    queryFn: async () => {
+      const { data } = await axiosCommon.get("/newsletter");
+      return data;
+    },
+  });
+  if (isLoading) return <Spinner />;
+
   return (
     <div>
       <Typography variant="h2" color="teal">
-        All Newsletter Subscribers
+        All Newsletter Subscribers {subscribers.length}
       </Typography>
       <div>
         <div className="text-gray-900 ">
@@ -17,47 +29,32 @@ const DashStat = () => {
             <table className="w-full text-md bg-white shadow-md rounded mb-4">
               <tbody>
                 <tr className="">
+                  <th className="text-left p-3 px-5">Numbers</th>
                   <th className="text-left p-3 px-5">Name</th>
                   <th className="text-left p-3 px-5">Email</th>
-
                   <th></th>
                 </tr>
-                <tr className=" hover:bg-orange-100 bg-gray-100">
-                  <td className="p-3 px-5">
-                    <input
-                      type="text"
-                      value="user.name"
-                      className="bg-transparent border-0"
-                      disabled
-                    />
-                  </td>
-                  <td className="p-3 px-5">
-                    <input
-                      type="text"
-                      value="user.email"
-                      disabled
-                      className="bg-transparent border-0"
-                    />
-                  </td>
-                </tr>
-                <tr className=" hover:bg-orange-100">
-                  <td className="p-3 px-5">
-                    <input
-                      type="text"
-                      value="user.name"
-                      disabled
-                      className="bg-transparent border-0"
-                    />
-                  </td>
-                  <td className="p-3 px-5">
-                    <input
-                      type="text"
-                      value="user.email"
-                      disabled
-                      className="bg-transparent border-0"
-                    />
-                  </td>
-                </tr>
+                {subscribers?.map((sub, idx) => (
+                  <tr key={idx} className=" hover:bg-orange-100 bg-gray-100">
+                    <td className="p-3 px-5">{idx + 1}</td>
+                    <td className="p-3 px-5">
+                      <input
+                        type="text"
+                        value={sub.name}
+                        className="bg-transparent border-0"
+                        disabled
+                      />
+                    </td>
+                    <td className="p-3 px-5">
+                      <input
+                        type="text"
+                        value={sub.email}
+                        disabled
+                        className="bg-transparent border-0"
+                      />
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
