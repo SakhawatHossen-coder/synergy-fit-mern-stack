e075947614f98dc1972ec3c0cd4e44a43518274f
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-06-04 20:51:05 +0600

    newsletter functionality done

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,17 +1,37 @@
 import { Button } from "@material-tailwind/react";
 import React from "react";
 import { useForm } from "react-hook-form";
+import useAxios from "../hooks/useAxios";
+import { toast } from "react-toastify";
 
 const Newsletter = () => {
-     const {
+  let axiosCommon = useAxios();
+  const {
     register,
     handleSubmit,
     watch,
+    reset,
     formState: { errors },
   } = useForm();
   const onSubmit = async (data) => {
     let { name, email } = data;
-  }
+    let subscriberInfo = {
+      name,
+      email,
+    };
+    try {
+      let result = await axiosCommon.post("/newsletter", subscriberInfo);
+      //  console.log(result);
+      //  throw new Error("This is a custom error message!");
+      if (result.data.insertedId) {
+        toast.success("Newsletter Subscribed Done!");
+      }
+      reset();
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  };
   return (
     <div>
       <div className="mt-16 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -31,16 +51,16 @@ const Newsletter = () => {
               className="sm:flex space-y-2"
               onSubmit={handleSubmit(onSubmit)}
             >
-              <label for="name" className="sr-only">
+              <label htmlFor="name" className="sr-only">
                 User Name
               </label>
-              <label for="email-address" className="sr-only">
+              <label htmlFor="email-address" className="sr-only">
                 Email address
               </label>
               <input
                 id="name"
                 type="text"
-                autocomplete="name"
+                autoComplete="name"
                 className="w-full rounded-md sm:mr-3 border-white px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-700"
                 placeholder="Your name"
                 {...register("name", { required: true })}
@@ -48,7 +68,7 @@ const Newsletter = () => {
               <input
                 id="email-address"
                 type="email"
-                autocomplete="email"
+                autoComplete="email"
                 className="w-full rounded-md border-white px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-700"
                 placeholder="Your email"
                 {...register("email", { required: true })}
@@ -68,12 +88,6 @@ const Newsletter = () => {
                 Subscribe
               </Button>
             </form>
-            <p className="mt-3 text-sm text-gray-100">
-              We care about the protection of your data. Read our
-              <a href="" className="font-medium text-white underline">
-                Privacy Policy.
-              </a>
-            </p>
           </div>
         </div>
       </div>
