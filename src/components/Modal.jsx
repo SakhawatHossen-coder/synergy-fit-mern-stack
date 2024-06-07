e678ac31b0ac7834c3279c93050216f1f70f231a
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-06-07 11:59:24 +0600

    modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,93 +1,127 @@
 import React, { useState } from "react";
+import {
+  Dialog,
+  Transition,
+  TransitionChild,
+  DialogTitle,
+  DialogPanel,
+} from "@headlessui/react";
+import { Fragment } from "react";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import { useMutation } from "@tanstack/react-query";
+import useAuth from "../hooks/useAuth";
+const Modal = ({
+  closeModal,
+  isOpen,
+  isModalOpen,
+  setIsOpen,
+  fullName,
+  email,
+  userRole,
+  age,
+  weekDays,
+  refetch,
+}) => {
+  // const [isOpen, setIsOpen] = useState(false);
 
-const Modal = () => {
-  
+  const {user}=useAuth()
+  const axiosSecure = useAxiosSecure();
+  const { mutateAsync } = useMutation({
+    mutationFn: async (role) => {
+      const { data } = await axiosSecure.patch(
+        `/trainer/update/${email}`,
+        role
+      );
+      return data;
+    },
+    onSuccess: (data) => {
+      refetch();
+      console.log(data);
+      toast.success("User role updated successfully!");
+      setIsOpen(false);
+    },
+  });
+  const modalHandler = async (selected) => {
+    console.log("I want to be a trainer");
+    if (user.email === email) {
+      toast.error("Action Not Allowed");
+      return setIsOpen(false);
+    }
 
+    const userRole = {
+      role: "Trainer",
+      status: "Verified",
+    };
+
+    try {
+      await mutateAsync(userRole);
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
+  };
   return (
-    <>
-      <main className="flex items-center justify-center w-screen h-screen flex-col bg-gray-100">
-        <button
-          type="button"
-          className="flex items-center justify-center px-4 font-medium bg-violet-700 text-white h-9 rounded-md hover:bg-violet-800 transition-all duration-300"
-          onClick={handleOpenModal}
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <TransitionChild
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <span className="flex items-center justify-center space-x-2">
-            Open modal
-          </span>
-        </button>
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </TransitionChild>
 
-        {showModal && (
-          <div
-            role="dialog"
-            id="modal-example"
-            aria-hidden={!showModal}
-            className={`modal fixed top-0 left-0 z-50 w-screen h-screen bg-black/30 flex items-center flex-col justify-center p-6 ${
-              showModal ? "show" : "fade"
-            }`}
-            tabIndex="-1"
-          >
-            <div
-              className="absolute top-0 left-0 z-[0] w-full h-full"
-              tabIndex="-1"
-            />
-            <article
-              className="modal-content flex flex-col relative m-0 rounded-md bg-white sm:my-16"
-              aria-labelledby="modal-title"
-              aria-describedby="modal-body"
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <TransitionChild
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
             >
-              <header className="flex p-4 items-center justify-between">
-                <h2 className="m-0 text-xl font-medium max-w-[calc(100%_-_3rem)]">
-                  Modal title
-                </h2>
-                <button
-                  type="button"
-                  className="flex items-center justify-center w-8 h-8 rounded-full bg-transparent transition-colors duration-300 hover:bg-black/10"
-                  onClick={handleCloseModal}
-                  aria-label="Close"
+              <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <DialogTitle
+                  as="h3"
+                  className="text-lg font-medium text-center leading-6 text-gray-900"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="22"
-                    height="22"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="#000000"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
+                  Become A Trainer!
+                </DialogTitle>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">Name: {fullName}</p>
+                  <p className="text-sm text-gray-500">Age: {age}</p>
+                  <p className="text-sm text-gray-500">Email: {email}</p>
+                  <p className="text-sm text-gray-500">Week Days: {weekDays}</p>
+                </div>
+                <hr className="mt-8 " />
+                <div className="flex mt-2 justify-around">
+                  <button
+                    type="button"
+                    onClick={modalHandler}
+                    className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
                   >
-                    <line x1="18" y1="6" x2="6" y2="18" />
-                    <line x1="6" y1="6" x2="18" y2="18" />
-                  </svg>
-                </button>
-              </header>
-              <main className="relative flex-[1_1_auto] p-4">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing…</p>
-              </main>
-              <footer className="flex flex-shrink-0 flex-wrap items-center justify-end flex-row p-4 gap-1">
-                <button
-                  type="button"
-                  className="flex items-center justify-center px-4 font-medium bg-gray-200 text-black h-9 rounded-md hover:bg-gray-300 transition-all duration-300"
-                  onClick={handleCloseModal}
-                >
-                  <span className="flex items-center justify-center space-x-2">
-                    Close
-                  </span>
-                </button>
-                <button
-                  type="button"
-                  className="flex items-center justify-center px-4 font-medium bg-violet-700 text-white h-9 rounded-md hover:bg-violet-800 transition-all duration-300"
-                >
-                  <span className="flex items-center justify-center space-x-2">
-                    Save changes
-                  </span>
-                </button>
-              </footer>
-            </article>
+                    Confirm
+                  </button>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                    onClick={closeModal}
+                  >
+                    Reject
+                  </button>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
           </div>
-        )}
-      </main>
-    </>
+        </div>
+      </Dialog>
+    </Transition>
   );
 };
 
--- src/dashboard/AppliedTrainersAdmin.jsx
+++ src/dashboard/AppliedTrainersAdmin.jsx
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 import { Button, Card, Spinner, Typography } from "@material-tailwind/react";
 import useAxios from "../hooks/useAxios";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { GrAction } from "react-icons/gr";
 import { CiEdit } from "react-icons/ci";
+import Modals from "../components/Modal";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AppliedTrainersAdmin = () => {
   const TABLE_HEAD = ["Name", "Email", "Role", "Action"];
-  const [showModal, setShowModal] = useState(false);
+  // for modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleOpenModal = () => setShowModal(true);
-  const handleCloseModal = () => setShowModal(false);
   const axiosCommon = useAxios();
-  const { data: trainers = [], isLoading } = useQuery({
+  const {
+    data: trainers = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["trainers"],
     queryFn: async () => {
       const { data } = await axiosCommon.get(`/trainer`);
       return data;
     },
   });
+  // const delHand = () => {};
+
   if (isLoading) return <Spinner className="mx-auto" />;
   console.log(trainers);
   return (
@@ -45,47 +57,88 @@ const AppliedTrainersAdmin = () => {
             </tr>
           </thead>
           <tbody>
-            {trainers?.map(({ fullName, email, userRole }, index) => (
-              <tr key={index} className="even:bg-blue-gray-50/50">
-                <td className="p-4">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {fullName}
-                  </Typography>
-                </td>
-                <td className="p-4">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {email}
-                  </Typography>
-                </td>
-                <td className="p-4">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-normal"
-                  >
-                    {userRole}
-                  </Typography>
-                </td>
-                <td className="p-4">
-                  <Button
-                    variant="filled"
-                    color="teal"
-                    className=""
-                    onClick={handleOpenModal}
-                  >
-                    <CiEdit size={18} />
-                  </Button>
-                </td>
-              </tr>
-            ))}
+            {trainers?.map(
+              ({ fullName, email, userRole, weekDays, age }, index) => {
+                let join = "Age" + ":" + age + " " + "Email" + ":" + email;
+                return (
+                  <>
+                    <tr key={index} className="even:bg-blue-gray-50/50">
+                      <td className="p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {fullName}
+                        </Typography>
+                      </td>
+                      <td className="p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {email}
+                        </Typography>
+                      </td>
+                      <td className="p-4">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {userRole}
+                        </Typography>
+                      </td>
+                      <td className="p-4">
+                        <Button
+                          variant="filled"
+                          color="teal"
+                          className=""
+                          // onClick={() => setIsModalOpen(true)}
+                          onClick={() => {
+                            Swal.fire({
+                              title: `Are you sure? Make ${fullName} a Trainer`,
+                              text: join,
+                              icon: "warning",
+                              showCancelButton: true,
+                              confirmButtonColor: "#3085d6",
+                              cancelButtonColor: "#d33",
+                              confirmButtonText: "Confirm",
+                            }).then((result) => {
+                              if (result.isConfirmed) {
+                                Swal.fire({
+                                  title: "Deleted!",
+                                  text: "Your file has been deleted.",
+                                  icon: "success",
+                                });
+                              }
+                            });
+                          }}
+                        >
+                          <CiEdit size={18} />
+                        </Button>
+                      </td>
+                    </tr>
+                    <Modals
+                      fullName={fullName}
+                      email={email}
+                      userRole={userRole}
+                      weekDays={weekDays}
+                      refetch={refetch}
+                      age={age}
+                      key={index + 1}
+                      isModalOpen={isModalOpen}
+                      closeModal={closeModal}
+                      setIsOpen={setIsOpen}
+                      isOpen={isOpen}
+                      // modalHandler={modalHandler}
+                    />
+                    ;
+                  </>
+                );
+              }
+            )}
           </tbody>
         </table>
       </Card>
