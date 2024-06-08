a8b5b40e45b44a879b4ccfaa326c1626cfa290b8
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-06-08 12:02:25 +0600

    private route

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import { Spinner } from "@material-tailwind/react";
 
-const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
-}
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+  if (loading) {
+    return <Spinner className="w-full mx-auto" color="amber" />;
+  }
+  if (!user) {
+    return <Navigate to="/login" state={location?.pathname || "/"} />;
+  }
+  return <div>{children}</div>;
+};
 
-export default PrivateRoute
+export default PrivateRoute;
